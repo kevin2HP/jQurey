09a7dd0065cb2abc4cda0632034a394f803da72a
kevin2hp <kevinp0930@hotmail.com>
2020-07-21 07:39:01 +1000

    first commit

---
+++ base/js/script.js
@@ -0,0 +1,17 @@
+// $(function() {
+//   // jQuery goes here...
+
+//   // Uncomment this line to fade out the red box on page load
+//   // $(".red-box").fadeOut(2000);
+// });
+
+$(function(){
+  $('.blue-box').animate({
+    'margin-left':'200px',
+    'opacity': '0' ,
+    'height':'50px',
+    'width':'50px',
+    'margin-top':'25px'
+    
+  },1000)
+})
